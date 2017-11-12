233a046e02c86be5538bf925e28200092ce20e12
Marco Tello <muchahuev@hotmail.com>
2017-11-11 18:26:24 -0600

    adding the model and the initial migration

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=SamuraiData;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
